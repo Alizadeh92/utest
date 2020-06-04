5d9498d25e048314b6d98934cd0801f646dc0e4b
Alizadeh92 <65341826+Alizadeh92@users.noreply.github.com>
2020-06-04 20:07:31 +0400

    Add files via upload

---
+++ ConsoleApp3/Calcul.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Calcul
+    {
+        public double Summa(double a, double b)
+        {
+            return (a + b);
+        }
+
+        public double Minus(double a, double b)
+        {
+            return (a - b);
+        }
+        public double Umnoj (double a, double b)
+        {
+            return (a * b);
+        }
+        public double Delen ( double a, double b)
+        {
+            return a / b;
+        }
+        public double Koren ( double a)
+        {
+            return Math.Sqrt(a);
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Calcul cal = new Calcul();
+            Console.WriteLine("выберите действие\n 1 сумма \n 2 вычитание \n 3 умножение \n 4 деление \n 5 корень");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    try
+                    {
+                        Console.WriteLine("введите первое число");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.WriteLine("введите второе число");
+                        double b = double.Parse(Console.ReadLine());
+                        Console.WriteLine(cal.Summa(a, b));
+                    }
+                    catch( Exception error)
+                    {
+                        Console.WriteLine(error.Message);
+                    }
+                    break;
+                case "2":
+                    try
+                    {
+                        Console.WriteLine("введите первое число");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.WriteLine("введите второе число");
+                        double b = double.Parse(Console.ReadLine());
+                        Console.WriteLine(cal.Minus(a, b));
+                    }
+                    catch (Exception error)
+                    {
+                        Console.WriteLine(error.Message);
+                    }
+                    break;
+                case "3":
+                    try
+                    {
+                        Console.WriteLine("введите первое число");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.WriteLine("введите второе число");
+                        double b = double.Parse(Console.ReadLine());
+                        Console.WriteLine(cal.Umnoj(a, b));
+                    }
+                    catch (Exception error)
+                    {
+                        Console.WriteLine(error.Message);
+                    }
+                    break;
+                case "4":
+                    try
+                    {
+                        Console.WriteLine("введите первое число");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.WriteLine("введите второе число");
+                        double b = double.Parse(Console.ReadLine());
+                        Console.WriteLine(cal.Delen(a, b));
+                    }
+                    catch (Exception error)
+                    {
+                        Console.WriteLine(error.Message);
+                    }
+                    break;
+                case "5":
+                    try
+                    {
+                        Console.WriteLine("введите  число");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.WriteLine(cal.Koren(a));
+                    }
+                    catch (Exception error)
+                    {
+                        Console.WriteLine(error.Message);
+                    }
+                    break;
+
+
+
+
+
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
